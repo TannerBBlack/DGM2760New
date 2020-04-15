dca357ca22598820a8a0e832b5fc590ae8b64111
Tanner Black <tbb777@gmail.com>
2020-04-14 20:16:27 -0600

    finished unit 10

--- Unit10/js/script.js
+++ Unit10/js/script.js
@@ -2,24 +2,34 @@
 
 async function getHotelData() {
     try {
-        const response = await fetch('../hotel.json')
-        return await response.json()
+        const response = await fetch('hotel.json')
+        return await response.json();
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
 let hotelData = {}
 getHotelData().then(data => hotelData = data)
 
-document.querySelector('marriott').addEventListener('click', hotelInfo)
+let anchors = document.querySelectorAll("a")
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', hotelInfo)
+});
+
+// document.querySelector("#marriott").addEventListener('click', hotelInfo)
 
 function hotelInfo(event) {
     let hotelChoice = hotelData.hotels.find(hotel => {
         return event.target.id === hotel.name.toLowerCase()
     })
 
-    document.querySelector("#hotelName").textContent = `${hotelChoice.name}
-    Hotel`
-    
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name} Hotel`
+
+    document.querySelector("#address").textContent = `${hotelChoice.address}`
+    document.querySelector("#rooms").textContent = `${hotelChoice.rooms}`
+    document.querySelector("#gym").textContent = `${hotelChoice.gym}`
+    document.querySelector("#type").textContent = `${hotelChoice.roomTypes}`
+    document.querySelector("#picture").setAttribute('src', `images/${hotelChoice.picture}`)
 }
